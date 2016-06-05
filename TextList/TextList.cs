a1aa4a299ca1f696afdaee9a149c659cee597ac9
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-06-05 09:00:16 -0500

    Mostly working
    With oxford without comma and without oxford with comma are failing.

--- TextList/TextList.cs
+++ TextList/TextList.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace TextList
+namespace NeoSmart.ExtensionMethods
 {
-    public class TextList
-    {
-        public TextList()
+    public static class TextList
+    {   
+        [Flags] 
+        public enum Formatting
         {
+            Default = OxfordSeparator | Spaces,
+            OxfordSeparator = 1,
+            Spaces = 2,
+            None = 0
         }
-    }
-}
 
+        public static string ToTextList<T>(this IEnumerable<T> entities, Formatting formatting = Formatting.Default, string conjunction = "and", string separator = ",")
+        {
+            bool oxfordComma = (formatting & Formatting.OxfordSeparator) == Formatting.OxfordSeparator;
+
+            var sb = new StringBuilder();
+            Queue<T> buffer = new Queue<T>(4);
+            string space = ((formatting & Formatting.Spaces) == Formatting.Spaces) ? " " : string.Empty;
+
+            foreach (var e in entities)
+            {
+                buffer.Enqueue(e);
+                if (buffer.Count < 4)
+                {
+                    continue;
+                }
+
+                //three elements have been guaranteed put aside
+                var t = buffer.Dequeue();
+                sb.AppendFormat("{0}{1}{2}", t, separator, space);
+            }
+
+            if (buffer.Count >= 1)
+            {
+                sb.Append(buffer.Dequeue());
+            }
+
+            //remember, buffer count has dropped
+            if (buffer.Count == 1) //means guaranteed only ever two entries
+            {
+                if (string.IsNullOrEmpty(conjunction))
+                {
+                    sb.AppendFormat("{0}{1}", separator, space, buffer.Dequeue());
+                }
+                else
+                {
+                    sb.AppendFormat("{0}{1}{2}{3}", space, conjunction, space, buffer.Dequeue());
+                }
+            }
+            else if (buffer.Count == 2)
+            {
+                sb.AppendFormat("{0}{1}{2}{3}{4}", separator, space, buffer.Dequeue(), oxfordComma ? separator : string.Empty, space);
+                if (string.IsNullOrEmpty(conjunction))
+                {
+                    sb.Append(buffer.Dequeue());
+                }
+                else
+                {
+                    sb.AppendFormat("{0}{1}{2}", conjunction, space, buffer.Dequeue());
+                }       
+            }
+            return sb.ToString();
+        }
+    }    
+}
--- TextListTest/Program.cs
+++ TextListTest/Program.cs
@@ -1,75 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using NeoSmart.ExtensionMethods.TextList;
-
-namespace NeoSmart.ExtensionMethods.TextList
-{
-    [Flags] 
-    public enum Formatting
-    {
-        Default = OxfordSeparator | Spaces,
-        OxfordSeparator = 1,
-        Spaces = 2,
-        None = 0
-    }
-
-    public static class ExtensionMethods
-    {   
-
-        public static string ToTextList<T>(this IEnumerable<T> entities, Formatting formatting = Formatting.Default, string conjunction = "and", string separator = ",")
-        {
-            bool oxfordComma = (formatting & Formatting.OxfordSeparator) == Formatting.OxfordSeparator;
-
-            var sb = new StringBuilder();
-            Queue<T> buffer = new Queue<T>(4);
-            string space = ((formatting & Formatting.Spaces) == Formatting.Spaces) ? " " : string.Empty;
-
-            foreach (var e in entities)
-            {
-                buffer.Enqueue(e);
-                if (buffer.Count < 4)
-                {
-                    continue;
-                }
-
-                var t = buffer.Dequeue();
-                //three elements have been guaranteed put aside
-                sb.AppendFormat("{0}{1}{2}", t, separator, space);
-            }
-
-            if (buffer.Count >= 1)
-            {
-                sb.Append(buffer.Dequeue());
-            }
-            //remember, buffer count has dropped
-            if (buffer.Count == 1) //means guaranteed only ever two entries
-            {
-                if (string.IsNullOrEmpty(conjunction))
-                {
-                    sb.AppendFormat("{0}{1}", separator, space, buffer.Dequeue());
-                }
-                else
-                {
-                    sb.AppendFormat("{0}{1}{2}{3}", space, conjunction, space, buffer.Dequeue());
-                }
-            }
-            else if (buffer.Count == 2)
-            {
-                sb.AppendFormat("{0}{1}{2}{3}{4}", separator, space, buffer.Dequeue(), oxfordComma ? separator : string.Empty, space);
-                if (string.IsNullOrEmpty(conjunction))
-                {
-                    sb.Append(buffer.Dequeue());
-                }
-                else
-                {
-                    sb.AppendFormat("{0}{1}{2}", conjunction, space, buffer.Dequeue());
-                }       
-            }
-            return sb.ToString();
-        }
-    }    
-}
+using NeoSmart.ExtensionMethods;
 
 public class Test
 {
@@ -81,22 +13,24 @@ public class Test
         bool oxford = true;
         bool spaces = true;
 
-        Console.WriteLine("Empty list");
+        Console.WriteLine("***Empty list***");
         Console.WriteLine(names.ToTextList());
 
         for (int i = 0; i < 2; ++i)
         {
-            Console.WriteLine("With{0} oxford comma", oxford ? "" : "out");
             for (int j = 0; j < 2; ++j)
             {
-                Console.WriteLine("With{0} spaces", spaces ? "" : "out");
+                Console.WriteLine("***With{0} oxford comma, with{1} spaces***", oxford ? "" : "out", spaces ? "" : "out");
                 names.Clear();
+
                 foreach (var name in allNames)
                 {
                     names.Add(name);
-                    Formatting flags = (spaces ? Formatting.Spaces : Formatting.None) | (oxford ? Formatting.OxfordSeparator : Formatting.None);
-                    Console.WriteLine(" {0}", names.ToTextList(flags, (i + j) % 2 == 0? "and" : "", ","));
+                    var flags = (spaces ? TextList.Formatting.Spaces : TextList.Formatting.None) |
+                                (oxford ? TextList.Formatting.OxfordSeparator : TextList.Formatting.None);
+                    Console.WriteLine(" {0}", names.ToTextList(flags, (i + j) % 2 == 0 ? "and" : "", ","));
                 }
+
                 spaces = !spaces;
                 Console.WriteLine();
             }
