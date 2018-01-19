36f66b1f42bf0241aaa26f3fdc54a2df6fe74043
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-01-19 15:26:19 -0600

    Implement Contains() extension method for NameObjectCollectionBase

--- ExtensionMethods/NameValueCollectionExtensions.cs
+++ ExtensionMethods/NameValueCollectionExtensions.cs
@@ -27,5 +27,19 @@ namespace NeoSmart.ExtensionMethods
             string value;
             return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
         }
+
+        public static bool Contains(this NameObjectCollectionBase.KeysCollection keys, string key)
+        {
+            for (int i = 0; i < keys.Count; ++i)
+            {
+                var item = keys.Get(i);
+                if (item == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
